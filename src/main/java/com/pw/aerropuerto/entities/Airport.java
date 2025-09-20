a49b630d6a39286f52a828f6c5c6ddfa7cd2adab
luifdo <luifsanchez@gmail.com>
2025-09-19 19:16:47 -0500

    primer caso

--- src/main/java/com/pw/aerropuerto/entities/Airport.java
+++ src/main/java/com/pw/aerropuerto/entities/Airport.java
@@ -1,4 +1,24 @@
 package com.pw.aerropuerto.entities;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@Table(name = "airports")
 public class Airport {
+
+    @Id @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+    String Code;
+    String Name;
+    String City;
+
+    @Builder.Default
 }
--- src/main/java/com/pw/aerropuerto/entities/SeatInventory.java
+++ src/main/java/com/pw/aerropuerto/entities/SeatInventory.java
@@ -1,4 +1,13 @@
 package com.pw.aerropuerto.entities;
 
+@Entity
+@Table(name = "amenities")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SeatInventory {
+
+
 }

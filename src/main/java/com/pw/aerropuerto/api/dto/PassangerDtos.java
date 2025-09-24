633098d9fd014c5bca0d0aefe77572ac22aad9a1
ESTUDIANTES <sjaltamar@gmail.com>
2025-09-23 19:14:20 -0500

    Test restantes... (_8(|)

--- src/main/java/com/pw/aerropuerto/api/dto/PassangerDtos.java
+++ src/main/java/com/pw/aerropuerto/api/dto/PassangerDtos.java
@@ -1,4 +1,11 @@
 package com.pw.aerropuerto.api.dto;
 
+import java.io.Serializable;
+
 public class PassangerDtos {
+    public record PassengerCreateRequest(String name, String email, PassengerProfileDto profile) implements Serializable {
+    }
+    public record PassengerProfileDto(String phone, String countryCode) implements Serializable{}
+    public record PassengerUpdateRequest(String name, String email, PassengerProfileDto profile) implements Serializable{}
+    public record PassengerResponse(Long Id, String name, String email, PassengerProfileDto profile) implements Serializable{}
 }
